da4de90839f2b0d8375d79a437e94a758533c820
anuragkumar96 <s531372@nwmissouri.edu>
2019-12-30 22:47:37 -0600

    Creating a project structure.

---
+++ src/test/java/com/anurag/spring/EmployeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.spring.EmployeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
