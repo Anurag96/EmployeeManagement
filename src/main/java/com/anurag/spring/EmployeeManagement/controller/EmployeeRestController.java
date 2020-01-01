4eba91611503ca0dad078df9d5f4581d237ac11a
anuragkumar96 <s531372@nwmissouri.edu>
2020-01-01 09:33:31 -0600

    Worked on Controller : GET,POST,PUT,DELETE

--- src/main/java/com/anurag/spring/EmployeeManagement/controller/EmployeeRestController.java
+++ src/main/java/com/anurag/spring/EmployeeManagement/controller/EmployeeRestController.java
@@ -5,9 +5,7 @@ import com.anurag.spring.EmployeeManagement.entity.Employee;
 import com.anurag.spring.EmployeeManagement.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,42 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
 
         return employeeService.findAll();
+    }
+
+    //Expose "/employees" and return the list of employees
+    @GetMapping("/employees/{employeeId}")
+    public Employee findbyId(@PathVariable int employeeId){
+
+        Employee employee= employeeService.findbyID(employeeId);
+        if(employee==null){
+            throw new RuntimeException("Employee Id is not found "+ employee);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee){  //We know that employee data is comes in request body as JSON, we need to handle bindind that JSON over to our given object.
+    employee.setId(0);
+    employeeService.save(employee);
+    return  employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+
+        Employee tempEmployyee=employeeService.findbyID(employeeId);
+
+        if(tempEmployyee==null){
+            throw new RuntimeException("Employee id not found : "+ employeeId);
+        }
+        employeeService.deletebyId(employeeId);
+        return "Deleted employee id : "+employeeId;
     }
 
 }
--- src/main/java/com/anurag/spring/EmployeeManagement/entity/Employee.java
+++ src/main/java/com/anurag/spring/EmployeeManagement/entity/Employee.java
@@ -9,12 +9,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "employee")
-//@Getter
-//@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-//@ToString
-@Data
+
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +21,57 @@ public class Employee {
     private String lastName;
     @Column(name = "email")
     private String email;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
