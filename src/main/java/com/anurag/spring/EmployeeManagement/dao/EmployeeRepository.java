fb03ed61ecad6d18cb09ac49ef01dddf8ffbd495
anuragkumar96 <s531372@nwmissouri.edu>
2020-01-02 14:23:15 -0600

    Explicitly exposing this rest repository and changing the name of end-point.

--- src/main/java/com/anurag/spring/EmployeeManagement/dao/EmployeeRepository.java
+++ src/main/java/com/anurag/spring/EmployeeManagement/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.anurag.spring.EmployeeManagement.dao;
 
 import com.anurag.spring.EmployeeManagement.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "employees") //explicitly exposing this rest repository and changing the name of end-point.
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
